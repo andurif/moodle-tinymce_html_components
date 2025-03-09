06c3f7259791130d8f7d50d7277bdffd25ce08b4
Anthony DURIF <anthony.durif@uca.fr>
2025-03-09 22:03:10 +0100

    Update translation files

--- lang/es/tiny_html_components.php
+++ lang/es/tiny_html_components.php
@@ -15,85 +15,88 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * Strings for component 'tiny_html_components', language 'es'.
  *
- * @package  tiny_html_components
- * @author  2020 Anthony Durif
+ * @package tiny_html_components
+ * @author 2020 Anthony Durif
  * @copyright 2020 Anthony Durif, Université Clermont Auvergne
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'Componentes HTML para Tiny';
-$string['pluginname_help'] = 'Extensión para ayudar usted a agregar componentes (bootstrap por ejemplo) en el editor Tiny.';
-$string['addhtml_components'] = 'Agregar componentes HTML';
-$string['html_components:desc'] = 'Agregar componentes HTML';
-$string['html_components:title'] = 'Agregar componentes HTML';
-$string['title'] = 'Agregar componentes HTML';
-$string['insert'] = 'Agregar HTML';
-$string['button_dropdown'] = 'Agregar menú al botón';
-$string['button_dropdown_nb'] = 'Número de opciones en el menú:';
-$string['card_disposition_info'] = 'Número máximo lado a lado: XL (1), L (2), M (3), S (4)';
-$string['choice_legend'] = 'Elija el componente';
-$string['component'] = 'Componente :';
-$string['component_placeholder'] = 'Elija el componente...';
-$string['basic_components'] = 'Componentes básicos';
-$string['custom_components'] = 'Componentes personalizados';
-$string['custom_components_title'] = 'Componentes HTML personalizados';
-$string['custom_components_link'] = 'Administre sus componentes personalizados';
-$string['custom_components_construct'] = 'Construcción del componente';
-$string['custom_components_create'] = 'Crear un componente persoalizado';
-$string['custom_components_edit'] = 'Editar el componente';
-$string['custom_components_delete'] = 'Borrar el componente';
-$string['custom_components_name'] = 'Nombre del componente';
-$string['custom_components_name_help'] = 'Nombre que desea darle al componente. Este nombre se mostrará en la lista de componentes disponible en el complemento del editor.';
-$string['custom_components_content'] = 'Contenido (HTML) del componente';
-$string['custom_components_content_help'] = 'Contenido en HTML del componente que construirás lanzando el editor. Eso se insertará después de la selección del componente.';
-$string['custom_components_back'] = 'Volver a la lista de sus componentes personalizados';
-$string['custom_components_preview_msg'] = '<div class="alert alert-info">Vista previa disponible después de la validación !</div>';
-$string['alert'] = 'Alerta';
-$string['alert_type'] = 'Tipo de alerta :';
-$string['alert_info'] = 'Información (cyan)';
-$string['alert_warning'] = 'Advertencia (amarillo)';
-$string['alert_success'] = 'Exito (verde)';
-$string['alert_danger'] = 'Error/peligro (rojo)';
-$string['alert_primary'] = 'Información principal (azul)';
-$string['alert_secondary'] = 'Información secondaria (gris)';
-$string['alert_dark'] = 'Dark (negro)';
-$string['alert_close'] = 'Posibilidad de cerrar la alerta :';
-$string['card'] = 'Viñeta';
-$string['card_nb'] = 'Número de viñeta :';
-$string['card_display'] = 'Visualización de las viñetas :';
-$string['card_display_horizontal'] = 'Lado a lado';
-$string['card_display_vertical'] = 'En columna';
-$string['card_background'] = 'Tipo de fondo :';
-$string['card_image'] = 'Imagen';
-$string['card_classic'] = 'Classic (blanco)';
-$string['card_orientation'] = 'Orientación de las viñetas : ';
-$string['card_orientation_portrait'] = 'Retrato';
-$string['card_orientation_landscape'] = 'Paisaje';
-$string['card_disposition'] = 'Disposición de las viñetas : ';
-$string['card_disposition_line'] = ' por linea';
-$string['jumbotron'] = 'Jumbrotron';
 $string['accordion'] = 'Acordéon';
 $string['accordion_nb'] = 'Número de lineas :';
 $string['accordion_show'] = 'Dejar lineas ocultas';
-$string['nav'] = 'Menu de navigacion';
-$string['nav_nb'] = 'Número de iperenlaces :';
+$string['addhtml_components'] = 'Agregar componentes HTML';
+$string['alert'] = 'Alerta';
+$string['alert_close'] = 'Posibilidad de cerrar la alerta :';
+$string['alert_danger'] = 'Error/peligro (rojo)';
+$string['alert_dark'] = 'Dark (negro)';
+$string['alert_info'] = 'Información (cyan)';
+$string['alert_primary'] = 'Información principal (azul)';
+$string['alert_secondary'] = 'Información secondaria (gris)';
+$string['alert_success'] = 'Exito (verde)';
+$string['alert_type'] = 'Tipo de alerta :';
+$string['alert_warning'] = 'Advertencia (amarillo)';
+$string['basic_components'] = 'Componentes básicos';
 $string['button'] = 'Botón';
+$string['button_dropdown'] = 'Agregar menú al botón';
+$string['button_dropdown_nb'] = 'Número de opciones en el menú:';
 $string['button_nb'] = 'Número de botones :';
-$string['button_type'] = 'Tipo de botón :';
+$string['button_size'] = 'Talla de los botones :';
+$string['button_size_block'] = 'Bloc';
+$string['button_size_lg'] = 'Grande';
+$string['button_size_med'] = 'Medio';
+$string['button_size_sm'] = 'Pequeño';
 $string['button_style'] = 'Estilo de botón :';
 $string['button_style_full'] = 'Lleno';
 $string['button_style_outline'] = 'Contorno';
-$string['button_size'] = 'Talla de los botones :';
-$string['button_size_sm'] = 'Pequeño';
-$string['button_size_med'] = 'Medio';
-$string['button_size_lg'] = 'Grande';
-$string['button_size_block'] = 'Bloc';
 $string['button_tooltip'] = 'Agregar un tooltip :';
-$string['button_tooltip_pos'] = 'Posicion :';
-$string['button_tooltip_top'] = 'Sobre';
-$string['button_tooltip_right'] = 'Derecha';
 $string['button_tooltip_bottom'] = 'Abajo';
 $string['button_tooltip_left'] = 'Izquierda';
-$string['preview_legend'] = 'Avance';
+$string['button_tooltip_pos'] = 'Posicion :';
+$string['button_tooltip_right'] = 'Derecha';
+$string['button_tooltip_top'] = 'Sobre';
+$string['button_type'] = 'Tipo de botón :';
+$string['card'] = 'Viñeta';
+$string['card_background'] = 'Tipo de fondo :';
+$string['card_classic'] = 'Classic (blanco)';
+$string['card_display'] = 'Visualización de las viñetas :';
+$string['card_display_horizontal'] = 'Lado a lado';
+$string['card_display_vertical'] = 'En columna';
+$string['card_disposition'] = 'Disposición de las viñetas : ';
+$string['card_disposition_info'] = 'Número máximo lado a lado: XL (1), L (2), M (3), S (4)';
+$string['card_disposition_line'] = ' por linea';
+$string['card_image'] = 'Imagen';
+$string['card_nb'] = 'Número de viñeta :';
+$string['card_orientation'] = 'Orientación de las viñetas : ';
+$string['card_orientation_landscape'] = 'Paisaje';
+$string['card_orientation_portrait'] = 'Retrato';
+$string['choice_legend'] = 'Elija el componente';
+$string['component'] = 'Componente :';
+$string['component_placeholder'] = 'Elija el componente...';
+$string['custom_components'] = 'Componentes personalizados';
+$string['custom_components_back'] = 'Volver a la lista de sus componentes personalizados';
+$string['custom_components_construct'] = 'Construcción del componente';
+$string['custom_components_content'] = 'Contenido (HTML) del componente';
+$string['custom_components_content_help'] = 'Contenido en HTML del componente que construirás lanzando el editor. Eso se insertará después de la selección del componente.';
+$string['custom_components_create'] = 'Crear un componente persoalizado';
+$string['custom_components_delete'] = 'Borrar el componente';
+$string['custom_components_edit'] = 'Editar el componente';
+$string['custom_components_link'] = 'Administre sus componentes personalizados';
+$string['custom_components_name'] = 'Nombre del componente';
+$string['custom_components_name_help'] = 'Nombre que desea darle al componente. Este nombre se mostrará en la lista de componentes disponible en el complemento del editor.';
+$string['custom_components_preview_msg'] = '<div class="alert alert-info">Vista previa disponible después de la validación !</div>';
+$string['custom_components_title'] = 'Componentes HTML personalizados';
+$string['html_components:desc'] = 'Agregar componentes HTML';
+$string['html_components:title'] = 'Agregar componentes HTML';
+$string['insert'] = 'Agregar HTML';
+$string['jumbotron'] = 'Jumbrotron';
+$string['nav'] = 'Menu de navigacion';
+$string['nav_nb'] = 'Número de iperenlaces :';
+$string['pluginname'] = 'Componentes HTML para Tiny';
+$string['pluginname_help'] = 'Extensión para ayudar usted a agregar componentes (bootstrap por ejemplo) en el editor Tiny.';
 $string['preview_desc'] = 'El avance del componente le permite visualizar los componentes que se mostrarán en la página. Esta visualización puede ser un poco diferente al render final según el tema, las opciones, etc...';
+$string['preview_legend'] = 'Avance';
+$string['privacy:metadata'] = 'La extensión no almacena ningún dato personal.';
+$string['suggestion'] = '';
+$string['title'] = 'Agregar componentes HTML';
--- lang/fr/tiny_html_components.php
+++ lang/fr/tiny_html_components.php
@@ -15,85 +15,88 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * Strings for component 'tiny_html_components', language 'fr'.
  *
- * @package  tiny_html_components
- * @author  2020 Anthony Durif
+ * @package tiny_html_components
+ * @author 2020 Anthony Durif
  * @copyright 2020 Anthony Durif, Université Clermont Auvergne
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'Tiny d\'insertion de composants HTML';
-$string['pluginname_help'] = 'Plugin permettant d\insérer des éléments html (bootstrap par exemple) via Tiny.';
-$string['addhtml_components'] = 'Insertion de composants HTML';
-$string['html_components:desc'] = 'Insertion de composants HTML';
-$string['html_components:title'] = 'Insérer des composants HTML';
-$string['title'] = 'Insérer des composants HTML';
-$string['insert'] = 'Insérer le code HTML';
-$string['button_dropdown'] = 'Ajouter un menu au bouton';
-$string['button_dropdown_nb'] = 'Nombre d\'options dans le menu :';
-$string['card_disposition_info'] = 'Nombre maxi côte à côte: XL (1), L (2), M (3), S (4)';
-$string['choice_legend'] = 'Choix du composant';
-$string['component'] = 'Composant :';
-$string['component_placeholder'] = 'Choisir le composant...';
-$string['basic_components'] = 'Composants de base';
-$string['custom_components'] = 'Composants personnels';
-$string['custom_components_title'] = 'Composants HTML personnalisés';
-$string['custom_components_link'] = 'Gérer vos composants personnalisés';
-$string['custom_components_construct'] = 'Construction du composant';
-$string['custom_components_create'] = 'Créer un nouveau composant personnalisé';
-$string['custom_components_edit'] = 'Editer le composant';
-$string['custom_components_delete'] = 'Supprimer le composant';
-$string['custom_components_name'] = 'Nom du composant';
-$string['custom_components_name_help'] = 'Nom que vous voulez donner au composant. Ce nom apparaîtra au niveau de la liste déroulante dans le plugin disponible dans l\'édtieur.';
-$string['custom_components_content'] = 'Contenu (HTML) du composant';
-$string['custom_components_content_help'] = 'Contenu du composant en HTML que vous allez construite via l\'éditeur. C\'est ce qui sera inséré à la sélection du composant.';
-$string['custom_components_back'] = 'Revenir à la liste des composants personnalisés';
-$string['custom_components_preview_msg'] = '<div class="alert alert-info">Aperçu disponible après validation !</div>';
-$string['alert'] = 'Alerte';
-$string['alert_type'] = 'Type d\'alerte :';
-$string['alert_info'] = 'Information (cyan)';
-$string['alert_warning'] = 'Avertissement (jaune)';
-$string['alert_success'] = 'Succès (vert)';
-$string['alert_danger'] = 'Erreur/danger (rouge)';
-$string['alert_primary'] = 'Information principale (bleu)';
-$string['alert_secondary'] = 'Information secondaire (gris)';
-$string['alert_dark'] = 'Dark (noir)';
-$string['alert_close'] = 'Permettre la fermeture de l\'alerte :';
-$string['card'] = 'Vignette';
-$string['card_nb'] = 'Nombre de vignettes :';
-$string['card_display'] = 'Alignement des vignettes :';
-$string['card_display_horizontal'] = 'Côte à côte';
-$string['card_display_vertical'] = 'En liste';
-$string['card_background'] = 'Type de background :';
-$string['card_image'] = 'Image';
-$string['card_classic'] = 'Classique (blanc)';
-$string['card_orientation'] = 'Orientation des vignettes :';
-$string['card_orientation_portrait'] = 'Portrait';
-$string['card_orientation_landscape'] = 'Paysage';
-$string['card_disposition'] = 'Disposition des vignettes : ';
-$string['card_disposition_line'] = ' par ligne';
-$string['jumbotron'] = 'Jumbrotron';
 $string['accordion'] = 'Accordéon';
 $string['accordion_nb'] = 'Nombre de lignes :';
 $string['accordion_show'] = 'Laisser les éléments repliés';
-$string['nav'] = 'Menu navigation';
-$string['nav_nb'] = 'Nombre de liens :';
+$string['addhtml_components'] = 'Insertion de composants HTML';
+$string['alert'] = 'Alerte';
+$string['alert_close'] = 'Permettre la fermeture de l\'alerte :';
+$string['alert_danger'] = 'Erreur/danger (rouge)';
+$string['alert_dark'] = 'Dark (noir)';
+$string['alert_info'] = 'Information (cyan)';
+$string['alert_primary'] = 'Information principale (bleu)';
+$string['alert_secondary'] = 'Information secondaire (gris)';
+$string['alert_success'] = 'Succès (vert)';
+$string['alert_type'] = 'Type d\'alerte :';
+$string['alert_warning'] = 'Avertissement (jaune)';
+$string['basic_components'] = 'Composants de base';
 $string['button'] = 'Bouton';
+$string['button_dropdown'] = 'Ajouter un menu au bouton';
+$string['button_dropdown_nb'] = 'Nombre d\'options dans le menu :';
 $string['button_nb'] = 'Nombre de boutons :';
-$string['button_type'] = 'Type des boutons :';
+$string['button_size'] = 'Taille des boutons :';
+$string['button_size_block'] = 'Bloc';
+$string['button_size_lg'] = 'Large';
+$string['button_size_med'] = 'Normal';
+$string['button_size_sm'] = 'Petit';
 $string['button_style'] = 'Style des boutons :';
 $string['button_style_full'] = 'Plein';
 $string['button_style_outline'] = 'Contour';
-$string['button_size'] = 'Taille des boutons :';
-$string['button_size_sm'] = 'Petit';
-$string['button_size_med'] = 'Normal';
-$string['button_size_lg'] = 'Large';
-$string['button_size_block'] = 'Bloc';
 $string['button_tooltip'] = 'Ajouter un tooltip (info-bulle) :';
-$string['button_tooltip_pos'] = 'Positionnement :';
-$string['button_tooltip_top'] = 'Au-dessus';
-$string['button_tooltip_right'] = 'A droite';
 $string['button_tooltip_bottom'] = 'En-dessous';
 $string['button_tooltip_left'] = 'A gauche';
-$string['preview_legend'] = 'Aperçu';
+$string['button_tooltip_pos'] = 'Positionnement :';
+$string['button_tooltip_right'] = 'A droite';
+$string['button_tooltip_top'] = 'Au-dessus';
+$string['button_type'] = 'Type des boutons :';
+$string['card'] = 'Vignette';
+$string['card_background'] = 'Type de background :';
+$string['card_classic'] = 'Classique (blanc)';
+$string['card_display'] = 'Alignement des vignettes :';
+$string['card_display_horizontal'] = 'Côte à côte';
+$string['card_display_vertical'] = 'En liste';
+$string['card_disposition'] = 'Disposition des vignettes : ';
+$string['card_disposition_info'] = 'Nombre maxi côte à côte: XL (1), L (2), M (3), S (4)';
+$string['card_disposition_line'] = ' par ligne';
+$string['card_image'] = 'Image';
+$string['card_nb'] = 'Nombre de vignettes :';
+$string['card_orientation'] = 'Orientation des vignettes :';
+$string['card_orientation_landscape'] = 'Paysage';
+$string['card_orientation_portrait'] = 'Portrait';
+$string['choice_legend'] = 'Choix du composant';
+$string['component'] = 'Composant :';
+$string['component_placeholder'] = 'Choisir le composant...';
+$string['custom_components'] = 'Composants personnels';
+$string['custom_components_back'] = 'Revenir à la liste des composants personnalisés';
+$string['custom_components_construct'] = 'Construction du composant';
+$string['custom_components_content'] = 'Contenu (HTML) du composant';
+$string['custom_components_content_help'] = 'Contenu du composant en HTML que vous allez construite via l\'éditeur. C\'est ce qui sera inséré à la sélection du composant.';
+$string['custom_components_delete'] = 'Supprimer le composant';
+$string['custom_components_create'] = 'Créer un nouveau composant personnalisé';
+$string['custom_components_edit'] = 'Editer le composant';
+$string['custom_components_link'] = 'Gérer vos composants personnalisés';
+$string['custom_components_name'] = 'Nom du composant';
+$string['custom_components_name_help'] = 'Nom que vous voulez donner au composant. Ce nom apparaîtra au niveau de la liste déroulante dans le plugin disponible dans l\'édtieur.';
+$string['custom_components_preview_msg'] = '<div class="alert alert-info">Aperçu disponible après validation !</div>';
+$string['custom_components_title'] = 'Composants HTML personnalisés';
+$string['html_components:desc'] = 'Insertion de composants HTML';
+$string['html_components:title'] = 'Insérer des composants HTML';
+$string['insert'] = 'Insérer le code HTML';
+$string['jumbotron'] = 'Jumbrotron';
+$string['nav'] = 'Menu navigation';
+$string['nav_nb'] = 'Nombre de liens :';
+$string['pluginname'] = 'Tiny d\'insertion de composants HTML';
+$string['pluginname_help'] = 'Plugin permettant d\insérer des éléments html (bootstrap par exemple) via Tiny.';
 $string['preview_desc'] = 'L\'aperçu des éléments vous permet de vous faire une idée et de visualiser les composants qui seront ajoutés. Attention, celui-ci pourra diverger quelques peu du rendu final en fonction du thème, des options choisies, etc... ';
+$string['preview_legend'] = 'Aperçu';
+$string['privacy:metadata'] = 'Le plugin ne stocke aucune donnée personnelle.';
+$string['suggestion'] = '';
+$string['title'] = 'Insérer des composants HTML';
